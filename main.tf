c4012aa014384aee508bbe6b5bb86bcf7dab9ab8
Patryk Ciechański <ciechanski.patryk@outlook.com>
2023-06-06 15:30:53 +0200

    Adding more files

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "resource_group" {
-    name            = "testing-rg01"
-    location        = "North Europe"
+    name            = var.rg_name
+    location        = var.location
 }
 resource "azurerm_storage_account" "storage_account" {
-    name                    = "testingStorageAccount"
+    name                    = var.sa_name
     resource_group_name     = azurerm_resource_gorup.resource_group.name
 }
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "rg_name" {
+  type = string
+  default = "testing-rg01"
+}
+variable "location" {
+  type = string
+  default = "North Europe"
+}
+variable "sa_name" {
+  type = string
+  default = "testingStorageAccount"
+}
