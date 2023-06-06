863b6f94a4f18306eec3e09089855bd2d0c319de
Patryk Ciechański <ciechanski.patryk@outlook.com>
2023-06-06 15:42:02 +0200

    Added storage container creation

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "azurerm_storage_account" "storage_account" {
     name                    = var.sa_name
     resource_group_name     = azurerm_resource_gorup.resource_group.name
 }
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
