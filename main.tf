29557c3b79ee7b5757d51f3b8c40baba2b00ac7a
Patryk Ciechański <ciechanski.patryk@outlook.com>
2023-06-06 15:44:37 +0200

    Added network rules

--- main.tf
+++ main.tf
@@ -5,4 +5,9 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_storage_account" "storage_account" {
     name                    = var.sa_name
     resource_group_name     = azurerm_resource_gorup.resource_group.name
+    
+    network_rules {
+        default_action             = "Deny"
+        ip_rules                   = ["100.0.0.1"]
+    }
 }
